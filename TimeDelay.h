e657d7e7147524e4edd2ebab1952eec0daaa1779
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-11-19 10:48:30 -0700

    Update TimeDelay.h

--- TimeDelay.h
+++ TimeDelay.h
@@ -12,7 +12,7 @@ extern "C" {
 }
 #endif
 
-void delay_ms(uint16_t time_ms);
+void delay_ms(uint16_t, uint8_t);
 void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void);
 
 #endif	/* CHANGECLK_H */
