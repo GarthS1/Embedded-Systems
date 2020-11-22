d23052890450e8c4d1c43a10ff2479ebc9c97a1c
longtv2222 <long.tv2222@gmail.com>
2020-11-22 11:30:09 -0700

    Update main

--- D-Project 5/newmainXC16.c
+++ D-Project 5/newmainXC16.c
@@ -7,8 +7,18 @@
 
 
 #include "xc.h"
+#include "ChangeClk.h"
+#include "ADC.h"
 
 int main(void) {
+    NewClk(32);     //Use system clock 32kHz
+    TRISAbits.TRISA3 = 1; //Enable RA3 as ADC input 
+    
+    while(1) {
+        Disp2String("\r");
+        Disp2Dec(do_ADC());
+        Disp2String("          ");
+    }
     
     return 0;
 }
