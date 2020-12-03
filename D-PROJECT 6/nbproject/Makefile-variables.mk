60d131c6fc7e45e05eba5dd95deefd7a1809ec8e
longtv2222 <long.tv2222@gmail.com>
2020-12-02 17:50:44 -0700

    Initial commit for D-PROJECT 6

---
+++ D-PROJECT 6/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=D-PROJECT_6.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/D-PROJECT_6.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=d-project6.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/d-project6.x.tar
---
+++ D-PROJECT 6/newmainXC16.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmainXC16.c
+ * Author: blaob
+ *
+ * Created on December 2, 2020, 5:48 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
