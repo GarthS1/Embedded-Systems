1aca597f8837d63fbb20df3d79ca637e55f64622
longtv2222 <long.tv2222@gmail.com>
2020-12-03 21:02:51 -0700

    Add nescessary files for project

--- App2.X/newmainXC16.c
+++ App2.X/newmainXC16.c
@@ -11,7 +11,11 @@
 #pragma config FCKSM = CSECMD // Clock switching is enabled, clock monitor disabled
 #pragma config OSCIOFNC = ON //CLKO output disabled on pin 8, use as IO.
 
-
+#define Nop() {__asm__ volatile ("nop");}
+#define ClrWdt() {__asm__ volatile ("clrwdt");}
+#define Sleep() {__asm__ volatile ("pwrsav #0");}   //Sleep() - put MCU in sleep mode - CPU and some peripherals off
+#define Idle() {__asm__ volatile ("pwrsav #1");}    //Idle() - put MCU in idle mode - only CPU off
+#define dsen() {__asm__ volatile ("BSET DSCON, #15");} //
 
 int main(void) {
     return 0;
